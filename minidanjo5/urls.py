ffe2ef45b39159c7fdd7c79e9854c7c8342f1aa6
chouguledeepa <chouguledeepa99@gmail.com>
2023-01-11 23:06:16 +0530

    second commit

--- minidanjo5/urls.py
+++ minidanjo5/urls.py
@@ -1,5 +1,4 @@
-"""minidanjo5 URL Configuration
-
+"""mini_django_v4 URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -13,15 +12,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include, re_path
-#from django.conf.urls import url
+
 
 from jobs import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    #path("", views.welcome)
-
-    re_path(r"^wel*", views.welcome)
+    path('admin/', admin.site.urls),
+    # path("", views.welcome)
+    path("jobs/", include("jobs.urls"))
 ]
